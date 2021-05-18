f7ad55ad5c1636e7ccc994d8e3a8931f43402be0
Lucas Le Moine <lucas.le-moine@etu.utc.fr>
2021-05-18 15:41:02 +0200

    initialisation projet

---
+++ projet/src/main/MachineMain.java
@@ -0,0 +1,47 @@
+package main;
+
+
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+
+public class MachineMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		startWithProfile();
+	}
+
+	public static void startWithProfile() {
+		Runtime rt = Runtime.instance();
+		Profile p = null;
+		ContainerController cc;
+		try {
+			/**
+			 * host : null value means use the default (i.e. localhost) port - is the port
+			 * number. A negative value should be used for using the default port number.
+			 * platformID - is the symbolic name of the platform, isMain : boolean
+			 */
+
+			p = new ProfileImpl(null, -1, "tdia04", false);
+
+			cc = rt.createAgentContainer(p);
+
+			AgentController ac = cc.createNewAgent("Machine", "td3.agents.MachineAgent", null);
+			ac.start();
+			ac = cc.createNewAgent("Client1", "td3.agents.ClientAgent", null);
+			ac.start();
+			ac = cc.createNewAgent("Client2", "td3.agents.ClientAgent", null);
+			ac.start();
+			ac = cc.createNewAgent("Client3", "td3.agents.ClientAgent", null);
+			ac.start();
+		} 
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
---
+++ projet/src/main/MainBoot.java
@@ -0,0 +1,31 @@
+package main;
+
+
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+
+public class MainBoot {
+	public static String MAIN_PROPERTIES_FILE = "properties/main.properties";
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		boot_gui();
+	}
+
+	public static void boot_gui() {
+		// open main console gui
+		// properties: main=true; gui = true;
+		Runtime rt = Runtime.instance();
+		Profile p = null;
+		try {
+			p = new ProfileImpl(MAIN_PROPERTIES_FILE);
+			rt.createMainContainer(p);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+}
